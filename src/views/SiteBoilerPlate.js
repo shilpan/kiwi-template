ca2e082f77d2f8a0a95fd58f92cc9d3b9fde4b85
shilpan <shilpanbhagat@gmail.com>
2014-02-13 11:51:35 -0500

    Turning off deferred javascript loading

--- src/views/SiteBoilerPlate.js
+++ src/views/SiteBoilerPlate.js
@@ -2,7 +2,7 @@
  * @jsx React.DOM
  */
 
-var React = require('React');
+var React = require('react');
 
 var SiteBoilerPlate = React.createClass({
   render: function() {
@@ -13,7 +13,7 @@ var SiteBoilerPlate = React.createClass({
           <title>Kiwi framework</title>
           <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"/>
           <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" />
-          <script src="/init.js" />
+          <script src="/bundle.js" />
         </head>
         {this.props.children}
       </html>
