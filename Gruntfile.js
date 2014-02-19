b21ef186042da4c426f83fa6850bef7bf5214c4e
smirea <steven.mirea@gmail.com>
2014-02-19 01:14:32 -0800

    lets name all absurd files *.css.js so we can distinguish them

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,11 +2,11 @@ module.exports = function(grunt) {
   grunt.initConfig({
     absurd: {
       css: {
-        src: __dirname + '/src/absurd/index.js',
+        src: __dirname + '/src/absurd/index.css.js',
         dest: __dirname + '/src/res/styles/styles.css'
       },
       cssmin: {
-        src: __dirname + '/src/absurd/index.js',
+        src: __dirname + '/src/absurd/index.css.js',
         dest: __dirname + '/src/res/styles/styles.min.css',
         options: {
           minify: true
